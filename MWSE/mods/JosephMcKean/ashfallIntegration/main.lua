77317622db0db3ab483b99d00b6b41d303b9a214
Amalie <amaliegay@outlook.com>
2023-04-30 10:47:53 +0800

    delete equipment requirements patch

--- MWSE/mods/JosephMcKean/ashfallIntegration/main.lua
+++
@@ -1,6 +0,0 @@
-local EquipmentRequirements = include("EquipmentRequirements.data")
-if EquipmentRequirements then
-	EquipmentRequirements.weaponReqs["ashfall_knife_flint"] = 5
-	EquipmentRequirements.weaponReqs["ashfall_woodaxe_flint"] = 5
-	EquipmentRequirements.weaponReqs["ashfall_pickaxe_flint"] = 5
-end
